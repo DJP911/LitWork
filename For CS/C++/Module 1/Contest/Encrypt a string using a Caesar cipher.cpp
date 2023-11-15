9a1118d03b7daaa85497334663e3ef10a26a3402
DJP911 <133709592+DJP911@users.noreply.github.com>
2023-11-15 12:13:59 +0530

    Create Encrypt a string using a Caesar cipher.cpp
    Problem Statement
    Encrypt a string using a Caesar cipher
    You and your best friend, Emily, want to surprise your other friend, Alex, with a special gift for their birthday. To keep the gift a secret, you decide to create a secret code using a Caesar cipher. You choose a rotation of 4 letters for the encryption. The original message is: "The gift is hidden in the attic."
    Using the Caesar cipher with a rotation of 4 letters, what is the encrypted message for the secret gift code?
    
    Important Note: Ensure that you save your solution before progressing to the next question and before submitting your answer.
    
    Exercise-1
    input:
    AsdfGH
    
    output:
    EwhjKL
    
    Exercise-2
    input:
    LkjHgg
    
    output:
    PonLkk

---
+++ For CS/C++/Module 1/Contest/Encrypt a string using a Caesar cipher.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string encryptCaesarCipher(const string& message, int rotation) {
+    string encryptedMessage = "";
+
+    for (char ch : message) {
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+            encryptedMessage += ((ch - base + rotation) % 26) + base;
+        } else {
+            encryptedMessage += ch;
+        }
+    }
+
+    return encryptedMessage;
+}
+
+int main() {
+    // Exercise-1
+    string input1 = "AsdfGH";
+    string output1 = encryptCaesarCipher(input1, 4);
+    cout << "Exercise-1 Output: " << output1 << endl;
+
+    // Exercise-2
+    string input2 = "LkjHgg";
+    string output2 = encryptCaesarCipher(input2, 4);
+    cout << "Exercise-2 Output: " << output2 << endl;
+
+    return 0;
+}
